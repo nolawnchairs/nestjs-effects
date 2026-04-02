57482f412c88352d33f22958e89508799b0c07d6
nolawnchairs <nolawnchairs@gmail.com>
2026-04-02 12:21:44 +0700

    feat: add default Request type parameter

--- src/dispatched-effect.event.ts
+++ src/dispatched-effect.event.ts
@@ -7,7 +7,7 @@
  * @template T - The type of the event value
  * @template R - The type of the request object
  */
-export class DispatchedEffectEvent<T, R> {
+export class DispatchedEffectEvent<T, R = {}> {
   /**
    * @param {T} value - The value returned from the decorated method
    * @param {R} request - The request object from the execution context (e.g., Express request)
